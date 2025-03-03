e6eeef0e015fa8c7f42a3a198006f7a4641e75f3
RushZ <Creativearush@gmail.com>
2025-03-03 18:02:11 -0500

    Initial Commit

---
+++ ProTIvity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProTIvity
+//
+//  Created by Arush Gupta on 2025-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProTIvity/ProTIvityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProTIvityApp.swift
+//  ProTIvity
+//
+//  Created by Arush Gupta on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct ProTIvityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
